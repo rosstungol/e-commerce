2708c838e2d3978fd702e46ed08d935fa6f533aa
Ross Tungol <rossjstungol@gmail.com>
2024-07-11 23:24:48 +0800

    Fixes ProductCard button typo

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -40,7 +40,7 @@ export function ProductCard({
 			</CardContent>
 			<CardFooter>
 				<Button asChild size="lg" className="w-full">
-					<Link href={`/products/${id}/purchase`}>Purchse</Link>
+					<Link href={`/products/${id}/purchase`}>Purchase</Link>
 				</Button>
 			</CardFooter>
 		</Card>
