b322a3bbe99b0e9e23510f1b9e132b5ce7345956
Ross Tungol <rossjstungol@gmail.com>
2024-07-16 03:36:40 +0800

    Updates Nav styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils'
 
 export function Nav({ children }: { children: ReactNode }) {
 	return (
-		<nav className="fixed z-10 flex h-16 w-full items-center justify-center border-b bg-white px-4 shadow-sm">
+		<nav className="fixed z-10 flex h-14 w-full items-center justify-center border-b bg-white px-4 shadow-sm">
 			{children}
 		</nav>
 	)
@@ -19,8 +19,8 @@ export function NavLink(props: Omit<ComponentProps<typeof Link>, 'className'>) {
 		<Link
 			{...props}
 			className={cn(
-				'p-4 transition hover:text-primary focus-visible:text-primary',
-				pathname === props.href && 'text-primary'
+				'h-full p-4 transition hover:bg-slate-50 hover:text-primary focus-visible:bg-slate-50 focus-visible:text-primary',
+				pathname === props.href && 'border-b-2 border-primary text-primary'
 			)}
 		/>
 	)
